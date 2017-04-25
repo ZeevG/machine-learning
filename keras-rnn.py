751a84718f5ef4cc023eeb0566280c72e24f022b
Ze'ev Gilovitz <zgilovitz@atlassian.com>
2017-04-25 13:07:15 +1000

    fixed incorrect user of keras layers

--- keras-rnn.py
+++ keras-rnn.py
@@ -43,13 +43,14 @@ print "y: ", y
 model = Sequential()
 model.add(LSTM(384, input_shape=(x.shape[1], x.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(384))
+model.add(LSTM(384, return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(384))
+model.add(LSTM(384, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(384))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
 # define the checkpoint
 filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
